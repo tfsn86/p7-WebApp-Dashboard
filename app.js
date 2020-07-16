b660cba631f61e0ba1c1dececc9daf0a1d8d2d50
tfsn <tfsn86@gmail.com>
2020-07-16 21:52:23 +0200

    Messaging section

--- app.js
+++ app.js
@@ -143,3 +143,21 @@ let mobileChart = new Chart(mobileCanvas, {
 	data: mobileData,
 	options: mobileOptions,
 });
+
+// Messaging section
+const user = document.getElementById('userField');
+const message = document.getElementById('messageField');
+const send = document.getElementById('send');
+
+send.addEventListener('click', (e) => {
+	if (user.value === '' && message.value === '') {
+		alert('Please fill out user and message fields before sending');
+	} else if (user.value === '') {
+		alert('Please fill out user field before sending');
+	} else if (message.value === '') {
+		alert('Please fill out message field before sending');
+	} else {
+		alert(`Message successfully sent to: ${user.value}`);
+	}
+	e.preventDefault();
+});
